75954597f5548c1925ade089ab36043e5f61960e
Sai <saicharan13@gmail.com>
2019-06-06 18:59:48 +0530

    Add CSS Sourcemap

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass");
+const sourcemaps = require("gulp-sourcemaps");
 const browserSync = require("browser-sync").create();
 
 function reload(done) {
@@ -10,8 +11,10 @@ function reload(done) {
 function css() {
   return gulp
     .src("./assets/scss/main.scss")
+    .pipe(sourcemaps.init())
     .pipe(sass({ outputStyle: "compressed" }))
     .on("error", sass.logError)
+    .pipe(sourcemaps.write("."))
     .pipe(gulp.dest("./assets/css"))
     .pipe(browserSync.stream());
 }
